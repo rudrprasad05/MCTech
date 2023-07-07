6053e322ca8743c9708e2aaad113cc4b71450ff6
Rudr <rudrprasad@yahoo.com>
2023-07-07 13:39:19 +1200

    working

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,28 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				
+			  },
+			  height: {
+				"10v": "10vh",
+				"20v": "20vh",
+				"30v": "30vh",
+				"40v": "40vh",
+				"45v": "45vh",
+				"50v": "50vh",
+				"60v": "60vh",
+				"70v": "70vh",
+				"80v": "80vh",
+				"90v": "90vh",
+				"100v": "100vh",
+			},
+			scale: {
+                '-100': '-1',
+            },
+		},
+		
 	},
 	plugins: [],
 }
